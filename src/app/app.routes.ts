c5e97ed792902b0757c52ceb49243745c9715494
Yan-NG <yaninagarofalo@gmail.com>
2026-01-24 22:58:58 -0300

    YG- form users component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'addUser', pathMatch: 'full'},
+  {path: 'addUser', loadComponent: () => import('./components/form-users/form-users').then(m => m.FormUsers)},
+];
